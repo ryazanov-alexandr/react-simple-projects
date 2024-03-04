769fd16f50979db3110ac0c877c3996cb70d28e6
ryazanov-alexandr <ryazanov.a1exandr@yandex.ru>
2024-03-04 16:30:57 +0300

    create modal

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-import './index.scss'
+import { useState } from "react";
+import "./index.scss";
+import { Modal } from "./components/modal";
 
 function App() {
+  const [open, setOpen] = useState(false);
+
+  const openModal = () => {
+    setOpen(true);
+  };
+
+  const closeModal = () => {
+    setOpen(false);
+  };
+
   return (
     <div className="App">
-      
+      <button onClick={openModal} className="open-modal-btn">
+        ✨ Открыть окно
+      </button>
+      <Modal open={open} closeModal={closeModal}>
+        <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" />
+      </Modal>
     </div>
   );
 }
