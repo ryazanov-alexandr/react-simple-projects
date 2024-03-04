def026fb5a67ea5bcc036a14995e1072526e06ef
ryazanov-alexandr <ryazanov.a1exandr@yandex.ru>
2024-03-04 15:40:13 +0300

    init counter

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
+import { useState } from 'react';
 import './index.scss'
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
+  const incerment = () => { 
+    setCounter(counter + 1)
+  }
+
+  const decerment = () => { 
+    setCounter(counter - 1)
+  }
+
   return (
     <div className="App">
-      
+      <div>
+        <h1>Счетчик</h1>
+        <h2>{counter}</h2>
+        <button onClick={decerment}>- Минус</button>
+        <button onClick={incerment}>Плюс +</button>
+      </div>
     </div>
   );
 }
